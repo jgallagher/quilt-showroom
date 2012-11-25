ce5f413b87729b267c512b2030485434d02621b6
John Gallagher <johnkgallagher@gmail.com>
2012-11-24 20:40:15 -0500

    Use filepicker to automatically rescale fabrics.

--- webapp/public/js/upload-fabric-image.js
+++ webapp/public/js/upload-fabric-image.js
@@ -1,17 +1,53 @@
 filepicker.setKey('AlTJ9WeNMRyujGjGoZ5gMz');
 
+var orig_fpfile = null;
+
 //Setup Aviary
 var featherEditor = new Aviary.Feather({
 	//Get an api key for Aviary at http://www.aviary.com/web-key
 	apiKey: 'd5776eb93',
 	apiVersion: 2,
 	onSave: function(imageID, newURL) {
-		$('#image-fabrics').append('<li class="span4"><div class="thumbnail"><img src="'+newURL+'"/><p>'+$('#fabricname').val()+'</p></div></li>');
-		$('#no-image-fabrics').hide();
-		$.post("upload-fabric", {
-			"name": $('#fabricname').val(),
-			"url": newURL
+		var toremove = orig_fpfile;
+		var fabricname = $("#fabricname").val();
+		var input = {
+			url: newURL,
+			filename: "from-aviary.png",
+			mimetype: "image/png",
+		};
+		filepicker.remove(toremove);
+		filepicker.store(input, function(fpfile) {
+			console.log(fpfile);
+			filepicker.stat(fpfile, {width:true, height:true},
+				function(metadata) {
+					console.log(metadata);
+					var w = metadata.width;
+					var h = metadata.height;
+					var rescale = w;
+					if (h > w) {
+						rescale = h;
+					}
+					// rescale so max dimention is 128 pixels
+					rescale = 128 / rescale;
+					w = Math.round(w * rescale);
+					h = Math.round(h * rescale);
+					console.log("should rescale to "+w+"x"+h);
+					filepicker.convert(fpfile, {
+						width: w,
+						height: h,
+						fit: 'scale',
+					}, function(newFpfile) {
+						filepicker.remove(fpfile);
+						$('#image-fabrics').append('<li class="span4"><div class="thumbnail"><img src="'+newFpfile.url+'"/><p>'+fabricname+'</p></div></li>');
+						$('#no-image-fabrics').hide();
+						$.post("upload-fabric", {
+							"name": fabricname,
+							"url": newFpfile.url,
+						});
+					});
+				});
 		});
+		featherEditor.close();
 	},
 	appendTo: 'web_demo_pane'
 });
@@ -25,9 +61,11 @@ editPane.onclick = function(){
 	filepicker.pick({mimetype: 'image/*'}, function(fpfile) {
 		//Showing the preview
 		preview.src = fpfile.url;
+		orig_fpfile = fpfile;
 
 		//Launching the Aviary Editor
 		featherEditor.launch({
+			fileFormat: "png",
 			image: preview,
 			url: fpfile.url
 		});
